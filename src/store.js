d96f18e0e4666ad118e66966f364641b096e8698
Pavel <92670880+PavelVynnPV@users.noreply.github.com>
2021-10-31 11:17:16 +0100

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,9 +1,62 @@
-import {createStore} from 'redux';
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import {
+  UPDATE_FILMS,
+  UPDATE_GENRES,
+  PERSONAL_FILM,
+  SET_LOADING,
+} from "./action";
 
-function reducer() {
-
+function reducer(state = { inputValue: "" }, action) {
+  switch (action.type) {
+    case "inputSearch": {
+      return { ...state, inputValue: action.payload };
+    }
+    default: {
+      return state;
+    }
+  }
 }
 
-const store = createStore(reducer);
+function filmsReducer(
+  state = { films: [], personalFilm: [], favourites: [] },
+  action
+) {
+  switch (action.type) {
+    case UPDATE_FILMS: {
+      return { ...state, films: action.payload, loading: false };
+    }
+    case PERSONAL_FILM: {
+      return { ...state, personalFilm: action.payload };
+    }
+    case "favouriteFilms": {
+      return { ...state, favourites: action.payload };
+    }
+    case SET_LOADING: {
+      return { ...state, loading: true };
+    }
+    default:
+      return state;
+  }
+}
+
+function genresReducer(state = { genres: [] }, action) {
+  switch (action.type) {
+    case UPDATE_GENRES: {
+      return { ...state, genres: action.payload };
+    }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(
+  combineReducers({
+    search: reducer,
+    filmsReducer: filmsReducer,
+    genresReducer: genresReducer,
+  }),
+  applyMiddleware(thunk)
+);
 
 export default store;
