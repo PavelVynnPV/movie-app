af12ca44cd7a4a117b8a7e8986fc9a5f8f09273d
Pavel <92670880+PavelVynnPV@users.noreply.github.com>
2021-10-31 11:18:13 +0100

    Update Navbar.js

--- src/compoents/Navbar.js
+++ src/compoents/Navbar.js
@@ -1,26 +1,35 @@
-import { Link } from "react-router-dom";
-import styles from "./Navbar.module.css";
-import Icon from "./Icons";
-import React from "react";
+import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
+import Icon from './Icons';
+import React from 'react';
+import { Search } from "./search";
+import { useSelector, useDispatch } from "react-redux";
 
-function Navbar({ setSearch }) {
+function Navbar() {
+
+  const inputValue = useSelector((state) => state.inputValue);
+  const dispatch = useDispatch();
+
+  function handleSearchChange(event) {
+    dispatch({ type: "inputSearch", payload: event.target.value});
+  }
+
   return (
     <>
       <nav className={styles.navbar}>
         <div className={styles.content}>
-          <Link className={styles.favourite_link} to="/favouritelist">
+          <Link className={styles.favourite_link} to="/favouriteList">
             Favourite {Icon}
           </Link>
           <Link className={styles.popular} to="/">
             <h1>Popular Movies</h1>
           </Link>
-          <input
+          <Search
             className={styles.search}
-            type="text"
             placeholder="Movie name"
-            onChange={(event) => {
-              setSearch(event.target.value);
-            }}
+            value={inputValue}
+            labelText="Search "
+            onChange={handleSearchChange}
           />
         </div>
       </nav>
