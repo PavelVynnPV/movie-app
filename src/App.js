2dd006698d0b57fc2f800492d8ec033dc02f7dc4
Pavel <92670880+PavelVynnPV@users.noreply.github.com>
2021-10-31 11:16:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
-import React from "react";
-import { useState } from "react";
-import Navbar from "./compoents/Navbar";
-import Main from "./compoents/Main";
-import FavouriteList from "./compoents/FavouriteList";
-import PersonalFilmInfo from "./compoents/PersonalFilmInfo";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import Navbar from './compoents/Navbar';
+import Main from './compoents/Main';
+import FavouriteList from './compoents/FavouriteList';
+import PersonalFilmInfo from './compoents/PersonalFilmInfo';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
-  const [search, setSearch] = useState("");
-  const [favourites, setFavourites] = useState([]);
-
   return (
     <>
       <Router>
         <Switch>
-          <Route exact path="/favouritelist">
+          <Route exact path="/favouriteList">
             <FavouriteList
-              favourites={favourites}
-              setFavourites={setFavourites}
             />
           </Route>
           <Route exact path="/personalfilminfo/:id">
             <PersonalFilmInfo />
           </Route>
           <Route exact path="/">
-            <Navbar setSearch={setSearch} />
+            <Navbar/>
 
             <Main
-              search={search}
-              setFavourites={setFavourites}
-              favourites={favourites}
             />
           </Route>
         </Switch>
+        
       </Router>
     </>
   );
